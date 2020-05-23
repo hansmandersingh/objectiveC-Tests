cdd77acf774ab47274656dbe3f0354c9015ba712
Hans <hansmandersingh1998@gmail.com>
2020-05-23 14:04:05 -0500

    Initial Commit

---
+++ LearningObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearningObjC
+//
+//  Created by hansmander Singh on 2020-05-23.
+//  Copyright © 2020 hansmander Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
